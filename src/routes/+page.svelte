f70772d96629f907c8c99b9737be628d68d452d3
York <111544765+helloyork@users.noreply.github.com>
2023-01-13 16:51:24 -0800

    临时

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,72 +1,9 @@
-<link href="..\src\base.css" rel="stylesheet">
+<script>
+    import "../app.css";
+  </script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1 class="text-3xl font-bold underline text-gray-100">Hello world!</h1>
-
-<div class="flex -space-x-1 overflow-hidden">
-	<img
-		class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-6 w-6 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-</div>
-
-<div class="flex -space-x-2 overflow-hidden">
-	<img
-		class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-8 w-8 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-</div>
-
-<div class="flex -space-x-2 overflow-hidden">
-	<img
-		class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
-		alt=""
-	/>
-	<img
-		class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
-		src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-		alt=""
-	/>
-</div>
+  <h1 class="text-3xl font-bold underline text-gray-200">
+    Hello world!
+  </h1>
